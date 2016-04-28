583972b0ea9a1ca58cb7399f73fef1eff99722bc
John's Dashboard Extraordinaire <sax1johno@gmail.com>
2016-04-28 09:10:23 +0000

    Getting really close

--- httpAuth.js
+++ httpAuth.js
@@ -1,158 +1,188 @@
-
 // If you use this as a template, update the copyright with your own name.
 
 // Sample Node-RED node file
 
 
 module.exports = function(RED) {
-    "use strict";
-    // require any external libraries we may need....
-    //var foo = require("foo-library");
-    // Statics go here.
-    var passport = require('passport');
-    var cookieParser = require('cookie-parser');
-    var expressSession = require('express-session');
-    
-    passport.serializeUser(function(user, done) {
-        // done has an error as the first param, and the user string as the second.
-        done(null, JSON.stringify(user));
-    });
-    
-    passport.deserializeUser(function(user, done) {
-        var u = JSON.parse(user);
-        done(null, u);
-    });
-    
-    // The main node definition - most things happen in here
-    function HttpAuthNode(config) {
-        // Create a RED node
-        RED.nodes.createNode(this,config);
+        "use strict";
+        // require any external libraries we may need....
+        //var foo = require("foo-library");
+        // Statics go here.
+        var passport = require('passport');
+        var cookieParser = require('cookie-parser');
+        var session = require('client-sessions');
 
-        // copy "this" object in case we need it in context of callbacks of other functions.
-        var node = this;
+        // The main node definition - most things happen in here
+        function HttpAuthNode(config) {
+            // Create a RED node
+            RED.nodes.createNode(this, config);
 
-        // this.status({fill:"red",shape:"ring",text:"disconnected"});
+            // copy "this" object in case we need it in context of callbacks of other functions.
+            var node = this;
 
-        // Store local copies of the node configuration (as defined in the .html)
-        // this.topic = config.topic;
-        // Retrieve the config node
-        if (RED.settings.httpNodeRoot !== false) {
-            this.strategy = RED.nodes.getNode(config.strategy);
-            if (this.strategy) {
-                try {
-                    // Create the seneca instance here and then attach a client.
-                    this.strategyName = this.strategy.strategy;
-                    this.strategyConfig = this.strategy.strategyConfig;
-                    this.middlewareConfig = this.strategy.middlewareconfig;
-                    
-                    // Take the strategyconfig and configure the passport method
-                    var strategyLibrary = "passport-" + this.strategyName;
-                    var Strategy = require(strategyLibrary).Strategy;
-    
-                    // Yes, I'm aware eval is evil.  In this case, we're trusting the user as
-                    // they are most likely the programmer and already have teh powerz.
-                    var middlewareConfigFn;
-                    if (this.middlewareConfig) {
-                        eval("middlewareConfigFn = " + this.middlewareConfig);
-                    }
-                    
-                    var passportString ="passport.use(new Strategy(" + this.strategyConfig + "));";
-                    // console.log(passportString);
-                    eval(passportString);
-                    
-                    var app = RED.httpNode;
-                    app.use(cookieParser());
-                    app.use(expressSession({ "secret": "keyboard cat"}));
-                    app.use(passport.initialize());
-                    app.use(passport.session());
-                    
-                    node.on('input', function (msg) {
-                        passport.authenticate(this.strategyName, middlewareConfigFn, function(err, user, info) {
-                            if (err) {
-                                node.error(err);
-                                msg.error = {
-                                    message: "Authentication Error: " + e,
-                                    code: "general"
-                                }
-                                node.send(msg);
-                            }
-                            else if (!user) {
-                                node.warn("User not found");
-                                msg.error = {
-                                    message: "Authentication failed: user not found",
-                                    code: "user-not-found"
-                                }
-                                node.send(msg);
-                            } else {
-                                msg.req.login(user, function(err) {
-                                    if (err) {
-                                        node.error(err);
-                                        msg.error = {
-                                            message: "Unable to log in user: " + err,
-                                            code: "invalid-credentials"
-                                        }
-                                        node.send(msg);
-                                    } else {
-                                        // Just send the message over since the user is contained in the request
-                                        // on the message.
-                                        node.send(msg);
-                                    }
+            // this.status({fill:"red",shape:"ring",text:"disconnected"});
+
+            // Store local copies of the node configuration (as defined in the .html)
+            // this.topic = config.topic;
+            // Retrieve the config node
+            if (RED.settings.httpNodeRoot !== false) {
+                node.strategy = RED.nodes.getNode(config.strategy);
+                if (node.strategy) {
+                    try {
+                        // Create the seneca instance here and then attach a client.
+                        node.strategyName = node.strategy.strategy;
+                        node.strategyConfig = node.strategy.strategyConfig;
+                        node.middlewareConfig = node.strategy.middlewareconfig;
+                        node.sessionConfig = node.strategy.sessionConfig || {
+                            cookieName: 'session',
+                            secret: 'random_string_goes_here',
+                            duration: 30 * 60 * 1000,
+                            activeDuration: 5 * 60 * 1000,
+                        };
+
+                        // Take the strategyconfig and configure the passport method
+                        var strategyLibrary = "passport-" + node.strategyName;
+                        var Strategy = require(strategyLibrary).Strategy;
+
+                        // Yes, I'm aware eval is evil.  In this case, we're trusting the user as
+                        // they are most likely the programmer and already have teh powerz.
+                        var middlewareConfigFn;
+                        if (node.middlewareConfig) {
+                            eval("middlewareConfigFn = " + node.middlewareConfig);
+                        }
+
+                        // RED.httpNode.use(cookieParser());
+                        // RED.httpNode.use(passport.initialize());
+                        // RED.httpNode.use(passport.session());
+
+                        var passportString = "passport.use(new Strategy(" + node.strategyConfig + "));";
+                        console.log(passportString);
+                        eval(passportString);
+
+                        passport.serializeUser(function(user, done) {
+                            // done has an error as the first param, and the user string as the second.
+                            done(null, JSON.stringify(user));
+                        });
+
+                        passport.deserializeUser(function(user, done) {
+                            var u = JSON.parse(user);
+                            done(null, u);
+                        });
+
+                        node.on('input', function(msg) {
+                                // var sesh = 
+                                session(node.sessionConfig)(msg.req, msg.res, function(err) {
+                                    console.log("in session");
+                                    passport.initialize()(msg.req, msg.res, function(err) {
+                                        console.log("in initialize");
+                                        passport.session()(msg.req, msg.res, function(err) {
+                                            console.log("Inside of passport.session");
+                                            passport.authenticate(node.strategyName, {
+                                                passReqToCallback: true,
+                                                failWithError: true
+                                            }, function(err, user, info) {
+                                                console.log("Made it into authenticate");
+                                                // console.log("error is ", err);
+                                                console.log("user is ", user);
+                                                // console.log(req.user);
+                                                if (err) {
+                                                    node.error(err);
+                                                    msg.error = {
+                                                        message: err,
+                                                        code: "general"
+                                                    }
+                                                    node.send(msg);
+                                                }
+                                                else if (!user) {
+                                                    node.error("User not found");
+                                                    msg.error = {
+                                                        message: "User Not Found",
+                                                        code: "user-not-found"
+                                                    }
+                                                    node.send(msg);
+                                                }
+                                                else {
+                                                    console.log("Reached the login point");
+                                                    // console.log("req is ", require('util').inspect(1));
+                                                    msg.req.logIn(user, function(err) {
+                                                        console.log("Called msg.req.login");
+                                                        if (err) {
+                                                            console.log("error is ", err);
+                                                            msg.error = {
+                                                                message: err,
+                                                                code: "login-error"
+                                                            }
+                                                            node.send(msg);
+                                                        }
+                                                        else {
+                                                            console.log("User logged in");
+                                                            node.send(msg);
+                                                        }
+                                                    });
+                                                }
+                                            })(msg.req, msg.res);
+                                        });
+                                    });
                                 });
-                            }
-                        })(msg.req, msg.res);
-                        // msg.payload contains the command object.
-                        // in this example just send it straight on... should process it here really
-                        
-                        // this.client.act(msg.payload, function(err, result) {
-                        //     if (err) {
-                        //         node.error(err);
-                        //         node.status({fill:"green",shape:"dot",text:"connected"});
-                        //         node.send(err);
-                        //     } else {
-                        //         msg.payload.result = result;
-                        //         node.send(msg);
-                        //         node.status({fill:"green",shape:"dot",text:"connected"});
-                        //     }
-                        // });
-                    });
-            
-                    // node.on("close", function() {
-                        // Called when the node is shutdown - eg on redeploy.
-                        // Allows ports to be closed, connections dropped etc.
-                        // eg: node.client.disconnect();
-                    // });
-                        
-                        // node.on("node-status", function() {
-                        // })
-        
-                        // node.status({fill:"green",shape:"dot",text:"connected"});
-                } catch (e) {
-                    node.error(["Exception in authorization node", e, e.stack].join(' '));
-                    console.error(e);
-                    // this.status({file: "red", shape: "dot", text: "ERR: see debug panel"});
+                                // passport.authenticate(this.strategyName, { failWithError: true })(msg.req, msg.res, function(req, res) {
+                                //     console.log("Hello from the out-siiiiiiiide!!!!! ");
+                                //     console.log("Request is ", req);
+                                //     node.send(msg);
+                                // });
+                                // msg.payload contains the command object.
+                                // in this example just send it straight on... should process it here really
+
+                                // this.client.act(msg.payload, function(err, result) {
+                                //     if (err) {
+                                //         node.error(err);
+                                //         node.status({fill:"green",shape:"dot",text:"connected"});
+                                //         node.send(err);
+                                //     } else {
+                                //         msg.payload.result = result;
+                                //         node.send(msg);
+                                //         node.status({fill:"green",shape:"dot",text:"connected"});
+                                //     }
+                                // });
+                            });
+                            // node.on("close", function() {
+                            // Called when the node is shutdown - eg on redeploy.
+                            // Allows ports to be closed, connections dropped etc.
+                            // eg: node.client.disconnect();
+                            // });
+
+                            // node.on("node-status", function() {
+                            // })
+
+                            // node.status({fill:"green",shape:"dot",text:"connected"});
+                        }
+                        catch (e) {
+                            node.error(["Exception in authorization node", e, e.stack].join(' '));
+                            console.error(e);
+                            // this.status({file: "red", shape: "dot", text: "ERR: see debug panel"});
+                        }
+                    }
+                    else {
+                        // this.status({fill:"red",shape:"dot",text:"ERR: No Config Found"});
+                        node.warn("An HTTP Root node is required.");
+                    }
                 }
-            } else {
-                // this.status({fill:"red",shape:"dot",text:"ERR: No Config Found"});
-                node.warn("An HTTP Root node is required.");
             }
-        }
-    }
 
-    // Register the node by name. This must be called before overriding any of the
-    // Node functions.
-    RED.nodes.registerType("httpAuth",HttpAuthNode);
-    
-    
-    // Register the configuration node
-    function httpAuthConfig(n) {
-        RED.nodes.createNode(this,n);
-        this.strategy = n.strategy;
-        this.strategyConfig = n.strategyConfig;
-        this.middlwareConfig = n.middlewareConfig;
-        this.name = n.name;
-        // console.error("Config in confignode is ", n);
-    }
-    RED.nodes.registerType("auth_config",httpAuthConfig);
+            // Register the node by name. This must be called before overriding any of the
+            // Node functions.
+            RED.nodes.registerType("httpAuth", HttpAuthNode);
 
 
-};
+            // Register the configuration node
+            function httpAuthConfig(n) {
+                RED.nodes.createNode(this, n);
+                this.strategy = n.strategy;
+                this.strategyConfig = n.strategyConfig;
+                this.middlwareConfig = n.middlewareConfig;
+                this.name = n.name;
+                // console.error("Config in confignode is ", n);
+            }
+            RED.nodes.registerType("auth_config", httpAuthConfig);
+
+
+        };
