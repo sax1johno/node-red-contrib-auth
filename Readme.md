8e62bb5d804946d5a31ee96af7c4cec42eb69748
John's Dashboard Extraordinaire <sax1johno@gmail.com>
2016-04-30 06:57:16 +0000

    Updated the readme to reflect node purpose

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # Node-Red Auth Node
 
-This library uses the passport.js library to secure http nodes using configurable strategies.
+This library uses the passport.js library to secure http nodes using JsonWebToken.
 
 ### Version
 0.1.0
@@ -11,13 +11,6 @@ This library uses the passport.js library to secure http nodes using configurabl
 $ npm install node-red-contrib-auth
 ```
 node-red should automatically detect the new node upon restarting of the server.
-
-To add a new strategy, install that strategy through npm.  The name of the strategy in node-red will be
-the name of the strategy without the "passport-" prefix:
-
-ex: `npm install passport-facebook`
-
-The users' information will be available through the "msg.req.user" object.
 
 License
 ----
--- jsonWebToken.js
+++ jsonWebToken.js
@@ -4,92 +4,101 @@
 
 
 module.exports = function(RED) {
-        "use strict";
-        // require any external libraries we may need....
-        //var foo = require("foo-library");
-        // Statics go here.
-        // var session = require('client-sessions');
-        var jwt = require('jsonwebtoken');
+    "use strict";
+    // require any external libraries we may need....
+    //var foo = require("foo-library");
+    // Statics go here.
+    // var session = require('client-sessions');
+    var jwt = require('jsonwebtoken');
 
-        // The main node definition - most things happen in here
-        function JsonWebToken(config) {
-            // Create a RED node
-            RED.nodes.createNode(this, config);
+    // The main node definition - most things happen in here
+    function JsonWebToken(config) {
+        // Create a RED node
+        RED.nodes.createNode(this, config);
 
-            // copy "this" object in case we need it in context of callbacks of other functions.
-            var node = this;
+        console.log("config = ", config);
+        // copy "this" object in case we need it in context of callbacks of other functions.
+        var node = this;
 
-            // this.status({fill:"red",shape:"ring",text:"disconnected"});
+        // this.status({fill:"red",shape:"ring",text:"disconnected"});
 
-            // Store local copies of the node configuration (as defined in the .html)
-            // this.topic = config.topic;
-            // Retrieve the config node
-                node.on('input', function(msg) {
-                        // var sesh = 
-                        try {
-                            // If there's no token present, encode the payload and add a token.
-                            if (!msg.token) {
-                                var token = jwt.sign(msg.payload, node.secret);
-                                msg.token = token;
-                                node.send(msg);
-                            } else {
-                                // otherwise, decode the token in msg.token and place it in the payload.
-                                jwt.verify(msg.token, node.secret, function(err, decoded) {
-                                  if (!err) {
-                                      msg.payload = decoded;
-                                      node.send(msg);
-                                  } else {
-                                      node.error(err);
-                                      msg.error = err;
-                                      node.send(msg);
-                                  }
-                                });
-                                
+        // Store local copies of the node configuration (as defined in the .html)
+        // this.topic = config.topic;
+        // Retrieve the config node
+        // console.log(config);
+        node.config = RED.nodes.getNode(config.tokenconfig);
+        node.on('input', function(msg) {
+            if (node.config) {
+                node.secret = node.config.secret;
+                try {
+                    // If there's no token present, encode the payload and add a token.
+                    if (!msg.token) {
+                        if (!msg.payload) {
+                            msg.error = {
+                                "message": "No token or payload present",
+                                "code": "nothing-to-do"
                             }
-                        } catch (e) {
-                            node.error("error" + e);
-                            console.log("Error", e.stack);
+                        } else {
+                            var token = jwt.sign(msg.payload, node.secret);
+                            msg.token = token;
                         }
-                        // passport.authenticate(this.strategyName, { failWithError: true })(msg.req, msg.res, function(req, res) {
-                        //     console.log("Hello from the out-siiiiiiiide!!!!! ");
-                        //     console.log("Request is ", req);
-                        //     node.send(msg);
-                        // });
-                        // msg.payload contains the command object.
-                        // in this example just send it straight on... should process it here really
-
-                        // this.client.act(msg.payload, function(err, result) {
-                        //     if (err) {
-                        //         node.error(err);
-                        //         node.status({fill:"green",shape:"dot",text:"connected"});
-                        //         node.send(err);
-                        //     } else {
-                        //         msg.payload.result = result;
-                        //         node.send(msg);
-                        //         node.status({fill:"green",shape:"dot",text:"connected"});
-                        //     }
-                        // });
-                    });
-                    // node.on("close", function() {
-                    // Called when the node is shutdown - eg on redeploy.
-                    // Allows ports to be closed, connections dropped etc.
-                    // eg: node.client.disconnect();
-                    // });
+                        node.send(msg);
+                    }
+                    else {
+                        // otherwise, decode the token in msg.token and place it in the payload.
+                        jwt.verify(msg.token, node.secret, function(err, decoded) {
+                            if (!err) {
+                                msg.payload = decoded;
+                                node.send(msg);
+                            }
+                            else {
+                                node.error(err);
+                                msg.error = err;
+                                node.send(msg);
+                            }
+                        });
+    
+                    }
+                }
+                catch (e) {
+                    node.error("error" + e);
+                    msg.error = {
+                        "message": e,
+                        "code": "general"
+                    }
+                    node.send(msg);
+                }
+            } else {
+                node.error("Unable to get config");
+                msg.error = {
+                    "message": "Unable to get config node",
+                    "code": "bad-configuration"
+                }
+                node.send(msg)
             }
+        });
+        
+        // node.on("close", function() {
+        // Called when the node is shutdown - eg on redeploy.
+        // Allows ports to be closed, connections dropped etc.
+        // eg: node.client.disconnect();
+        // });
+    }
 
-            // Register the node by name. This must be called before overriding any of the
-            // Node functions.
-            RED.nodes.registerType("JsonWebToken", JsonWebToken);
-
-
-            // Register the configuration node
-            function JsonWebTokenConfig(n) {
-                RED.nodes.createNode(this, n);
-                this.name = n.name;
-                this.secret = n.secret;
-                // console.error("Config in confignode is ", n);
-            }
-            RED.nodes.registerType("token_config", JsonWebTokenConfig);
+    // Register the node by name. This must be called before overriding any of the
+    // Node functions.
+    RED.nodes.registerType("JsonWebToken", JsonWebToken);
 
 
-        };
+    // Register the configuration node
+    function JsonWebTokenConfig(n) {
+        RED.nodes.createNode(this, n);
+        this.name = n.name;
+        this.secret = n.secret;
+        console.log("Secret = ", n.secret);
+        // console.error("Config in confignode is ", n);
+    }
+    RED.nodes.registerType("JsonWebToken_config", JsonWebTokenConfig);
+
+
+};
